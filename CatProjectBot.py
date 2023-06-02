9063550055049bc4064691c031e8cb68354774f0
Aleksandr Kryvobok <phoenixcypres@gmail.com>
2023-06-02 19:42:25 +0300

    Update telegram bot

--- CatProjectBot.py
+++ CatProjectBot.py
@@ -1,15 +1,45 @@
+import random
 import telebot
 import logging
 from dotenv import dotenv_values
+import requests
+from bs4 import BeautifulSoup
+import wget
+import subprocess
+
 
 env_vars = dotenv_values('.env')
 telegram_bot_token = env_vars['bot_token']
 bot = telebot.TeleBot(telegram_bot_token)
 
+#def find_cat(command):
+#	urlcat = f'https://unsplash.com/s/photos/{command}?license=free'
 
-@bot.message_handler(commands=['Cat'])
-def send_welcome(message):
-	Photo = open('Cat1.jpg', 'rb')
-	bot.send_photo(message.chat.id, Photo)
+# @bot.message_handler(commands=['Cat'])
+# def send_welcome(message):
+# 	command = message.text.split()[0]
+# 	Photo = open('Cat1.jpg', 'rb')
+# 	bot.send_photo(message.chat.id, Photo)
+#
+
+@bot.message_handler(commands=['good-cat'])
+def send_good_cat(message):
+	command = message.text.split()[0]
+	respone = find_cat(command)
+	parsed_cat = parsecat(respone)
+	cat = random.choice(parsed_cat)
+	bot.send_message(message.chat.id, cat)
+
+def find_cat(command):
+	urlcat = f'https://unsplash.com/s/photos/{command}?license=free'
+	response = requests.get(urlcat)
+	html = response.content
+	return html
+
+def parsecat(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    download_links = soup.find_all('a', title='Download photo')
+    download_urls = [link['href'] for link in download_links]
+    return download_urls
 
 bot.infinity_polling()
--- main.py
+++ main.py
@@ -2,3 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 import wget
 
+# def find_cat():
+# 	urlcat = 'https://unsplash.com/s/photos/cat?license=free'
+#     response = requests.get(urlcat)
+#     html_content = response.content
+# 	return respone, html_content
+#
+# def parse_cat(url):
+# 	soup = BeautifulSoup(url, 'html.parser')
+# 	for link in soup.find_all('a'):
+# 		print(link.get('href'))
+
+
+
+def find_cat(url):
+    urlcat = url
+    response = requests.get(urlcat)
+    html = response.content
+    return html
+
+def parsecat(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    download_links = soup.find_all('a', title='Download photo')
+    download_urls = [link['href'] for link in download_links]
+    print(download_urls)
+    return download_urls
+
+
+url = 'https://unsplash.com/s/photos/cat?license=free'
+html = find_cat(url)
+cat = parsecat(html)
+
+print(cat)
+
